f9938a6a35edbf5bebe40dd98ecdb197150f8610
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2025-11-24 15:58:37 +0000

    Install webserver for Heroku, create Procfile, add required code for Heroku setup

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bed3inl8^s%0^gtz#$g=ql*w@p9j$l&yxm8xjfij5u!1kozg#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
